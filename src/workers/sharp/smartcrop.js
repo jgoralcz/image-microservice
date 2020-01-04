fb9eee49fb43b37478e5e89995d3ffad55b73f71
jgoralcz <jgoralcz@asu.edu>
2020-01-04 12:12:01 -0700

    updated to work with static images, not gifs

--- src/workers/sharp/smartcrop.js
+++ src/workers/sharp/smartcrop.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
 const cv = require('opencv');
-const gm = require('gm').subClass({ imageMagick: true });
+const im = require('gm').subClass({ imageMagick: true });
+const gm = require('gm');
 const smartcrop = require('smartcrop-gm');
 const sharp = require('sharp');
+
+const isGif = (buffer) => buffer.toString('hex', 0, 4) === '47494638';
 
 const faceDetect = (input, userOptions) => new Promise((resolve, reject) => {
   cv.readImage(input, (err, image) => {
@@ -28,20 +31,18 @@ const promiseGMBufferFirstFrame = (buffer, frame = 0) => new Promise((resolve, r
   });
 });
 
-const promiseGM = (buffer, crop, width, height) => new Promise((resolve, reject) => {
-  const imageType = buffer.toString('hex', 0, 4);
-
-  // magic numbers for gif detection
-  if (imageType === '47494638') {
-    return gm(buffer)
-      .quality(60)
-      .crop(crop.width * 1.6, crop.height * 1.6, crop.x, crop.y)
-      .gravity('Center')
+const promiseGM = (buffer, crop, width, height, gif) => new Promise((resolve, reject) => {
+  if (gif) {
+    return im(buffer)
+      .coalesce()
+      .crop(crop.width * 2, crop.height * 2, crop.x, crop.y)
+      // .gravity('Center')
       .resize(width * 2, height * 2)
       .resize(null, height)
-      // .extent(width, height)
-      .crop(width, height, 0)
-      .crop(width + 92, height - 4, -50, 2)
+      .crop(width, height, 0, 0)
+      .trim()
+      // .crop(width, height - 4, 0, 2)
+      .background('#ffffff')
       .borderColor('white')
       .border(2, 2)
       .toBuffer((err, buf) => {
@@ -50,13 +51,14 @@ const promiseGM = (buffer, crop, width, height) => new Promise((resolve, reject)
       });
   }
   return gm(buffer)
-    .quality(80)
     .gravity('Center')
-    .crop(crop.width * 1.3, crop.height * 1.3, crop.x, crop.y)
+    .crop(crop.width * 2, crop.height * 2, crop.x, crop.y)
     .resize(width * 2, height * 2)
     .resize(null, height)
     .crop(width, height, 0)
     .crop(width + 92, height - 4, -50, 2)
+    .background('#ffffff')
+    .extent(221, 346)
     .borderColor('white')
     .border(2, 2)
     .flatten()
@@ -67,8 +69,63 @@ const promiseGM = (buffer, crop, width, height) => new Promise((resolve, reject)
 });
 
 const execute = async (url, width, height, userOptions) => {
-  const { data: buffer } = await axios.get(url, { responseType: 'arraybuffer' });
+  let { data: buffer } = await axios.get(url, { responseType: 'arraybuffer' });
   if (!buffer) return undefined;
+
+  const metadata = await sharp(buffer).metadata();
+
+  if (metadata.width === 225 && metadata.height === 350) {
+    return new Promise((resolve, reject) => {
+      gm(buffer)
+        .background('#ffffff')
+        .extent(221, 346)
+        .borderColor('white')
+        .border(2, 2)
+        .flatten()
+        .toBuffer(isGif(buffer) ? 'gif' : 'jpg', (err, buf) => {
+          if (err) return reject(err);
+          return resolve(buf);
+        });
+    });
+  }
+
+  // another weird ratio hard to get image sometimes.
+  const ratio = metadata.width / metadata.height;
+  if (ratio <= 0.56) {
+    return new Promise((resolve, reject) => {
+      gm(buffer)
+        .coalesce()
+        .resize(225, metadata.height * (225 / metadata.width), '!')
+        .crop(221, 346, 0, 0)
+        .background('#ffffff')
+        .extent(221, 346)
+        .borderColor('white')
+        .border(2, 2)
+        .flatten()
+        .toBuffer(isGif(buffer) ? 'gif' : 'jpg', (err, buf) => {
+          if (err) return reject(err);
+          return resolve(buf);
+        });
+    });
+  }
+  if (ratio <= 0.64) {
+    return new Promise((resolve, reject) => {
+      gm(buffer)
+        .coalesce()
+        .gravity('Center')
+        .resize(225, metadata.height * (225 / metadata.width), '!')
+        .crop(221, 346, 0, 0)
+        .background('#ffffff')
+        .extent(221, 346)
+        .borderColor('white')
+        .border(2, 2)
+        .flatten()
+        .toBuffer(isGif(buffer) ? 'gif' : 'jpg', (err, buf) => {
+          if (err) return reject(err);
+          return resolve(buf);
+        });
+    });
+  }
 
   const firstFrameBuffer = await promiseGMBufferFirstFrame(buffer);
 
@@ -76,23 +133,12 @@ const execute = async (url, width, height, userOptions) => {
 
   const reduceQualityBuffer = (firstFrameBuffer.toString().length > 10000) ? await sharp(firstFrameBuffer).jpeg({ quality: 20 }).toBuffer() : firstFrameBuffer;
 
-
   const options = await faceDetect(reduceQualityBuffer, userOptions).catch(() => []) || [];
 
   const { topCrop: crop } = await smartcrop.crop(buffer, options);
-  const metadata = await sharp(buffer).metadata();
 
-  if (metadata.width % 225 === 0 && metadata.height % 350 === 0) {
-    return new Promise((resolve, reject) => {
-      gm(buffer).borderColor('white')
-        .resize(225, 350)
-        .border(2, 2)
-        .flatten()
-        .toBuffer('jpg', (err, buf) => {
-          if (err) return reject(err);
-          return resolve(buf);
-        });
-    });
+  if (isGif(buffer)) {
+    return promiseGM(buffer, crop, width, height, true);
   }
 
   // ratio weird with gm
@@ -100,10 +146,12 @@ const execute = async (url, width, height, userOptions) => {
     const sharpBuffer = await sharp(buffer)
       .extract({ width: crop.width, height: crop.height, left: crop.x, top: crop.y })
       .resize(width, height)
-      .jpeg({ quality: 100, progressive: true })
       .toBuffer();
     return new Promise((resolve, reject) => {
-      gm(sharpBuffer).borderColor('white')
+      gm(sharpBuffer)
+        .background('#ffffff')
+        .extent(221, 346)
+        .borderColor('white')
         .border(2, 2)
         .flatten()
         .toBuffer('jpg', (err, buf) => {
