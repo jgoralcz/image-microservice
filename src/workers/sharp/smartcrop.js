f68a2824348fd15c7d52bfda809e105126aac46e
jgoralcz <jgoralcz@asu.edu>
2020-01-10 13:37:12 -0700

    smartcrop no border finalized

--- src/workers/sharp/smartcrop.js
+++ src/workers/sharp/smartcrop.js
@@ -5,7 +5,16 @@ const gm = require('gm');
 const smartcrop = require('smartcrop-gm');
 const sharp = require('sharp');
 
-const isGif = (buffer) => buffer.toString('hex', 0, 4) === '47494638';
+const MAGIC = Object.freeze({
+  jpgNumber: 'ffd8ffe0',
+  jpg2Number: 'ffd8ffe1',
+  pngNumber: '89504e47',
+  gifNumber: '47494638',
+  jpgGeneral: 'ffd8ff',
+  webm: '1f45dfa3',
+});
+
+const isImageType = (buffer, type = MAGIC.gifNumber) => buffer.toString('hex', 0, 4) === type;
 
 const faceDetect = (input, userOptions) => new Promise((resolve, reject) => {
   cv.readImage(input, (err, image) => {
@@ -31,18 +40,17 @@ const promiseGMBufferFirstFrame = (buffer, frame = 0) => new Promise((resolve, r
   });
 });
 
-const promiseGM = (buffer, crop, width, height, multiplier, gif) => new Promise((resolve, reject) => {
+const promiseGM = (buffer, crop, width, height, gif) => new Promise((resolve, reject) => {
   if (gif) {
     return im(buffer)
       .coalesce()
       .gravity('Center')
-      .sharpen(2, 1)
+      .sharpen(1.5, 1)
       .crop(crop.width * 2, crop.height * 2, crop.x, crop.y)
       .resize(225 * 2, 350 * 2)
       .resize(null, 350)
       .borderColor('white')
-      .extent(225 - 4, 350 - 4)
-      .border(2, 2)
+      .extent(225, 350)
       .repage('+')
       .toBuffer((err, buf) => {
         if (err) return reject(err);
@@ -52,16 +60,14 @@ const promiseGM = (buffer, crop, width, height, multiplier, gif) => new Promise(
   return gm(buffer)
     .quality(92)
     .gravity('Center')
-    .sharpen(2, 1)
+    .sharpen(1.5, 1)
     .crop(crop.width * 2, crop.height * 2, crop.x, crop.y)
     .resize(width * 2, height * 2)
     .resize(null, height)
     .crop(width, height, 0)
-    .crop(width + 92, height - 4 * multiplier, -50, 2 * multiplier)
+    .crop(width + 100, height, -50, 0)
     .background('#ffffff')
-    .extent(width - 4 * multiplier, height - 4 * multiplier)
-    .borderColor('white')
-    .border(2 * multiplier, 2 * multiplier)
+    // .extent(width, height)
     .flatten()
     .toBuffer('jpg', (err, buf) => {
       if (err) return reject(err);
@@ -70,29 +76,34 @@ const promiseGM = (buffer, crop, width, height, multiplier, gif) => new Promise(
 });
 
 const execute = async (url, width, height, userOptions) => {
-  const { data: buffer } = await axios.get(url, { responseType: 'arraybuffer' });
+  let { data: buffer } = await axios.get(url, { responseType: 'arraybuffer' });
   if (!buffer) return undefined;
+
+  if (isImageType(buffer, MAGIC.pngNumber)) {
+    buffer = await new Promise((resolve, reject) => {
+      gm(buffer).trim()
+        .toBuffer('png', (err, buf) => {
+          if (err) return reject(err);
+          return resolve(buf);
+        });
+    });
+  }
 
   const metadata = await sharp(buffer).metadata();
   const roundedRatio = Math.floor((metadata.width / metadata.height) * 100) / 100;
 
-  // const multiplier = (width > 225 && height > 350) ? 1 : 0.5;
-  const multiplier = 0.5;
-
-  if ((roundedRatio === 0.63 || roundedRatio === 0.64 || roundedRatio === 0.65) && !isGif(buffer)) {
+  if ((roundedRatio === 0.63 || roundedRatio === 0.64 || roundedRatio === 0.65) && !isImageType(buffer, MAGIC.gifNumber)) {
     return new Promise((resolve, reject) => {
       gm(buffer)
         .background('#ffffff')
         .quality(92)
-        .sharpen(2, 1)
+        .sharpen(1.5, 1)
         .resize(width, height)
         .crop(width, height, 0, 0)
         .gravity('Center')
-        .extent(width - 4 * multiplier, height - 4 * multiplier)
-        .borderColor('white')
-        .border(2 * multiplier, 2 * multiplier)
+        .extent(width, height)
         .flatten()
-        .toBuffer(isGif(buffer) ? 'gif' : 'jpg', (err, buf) => {
+        .toBuffer(isImageType(buffer, MAGIC.gifNumber) ? 'gif' : 'jpg', (err, buf) => {
           if (err) return reject(err);
           return resolve(buf);
         });
@@ -100,38 +111,34 @@ const execute = async (url, width, height, userOptions) => {
   }
 
   // another weird ratio hard to get image sometimes.
-  if (roundedRatio <= 0.56 && !isGif(buffer)) {
+  if (roundedRatio <= 0.56 && !isImageType(buffer, MAGIC.gifNumber)) {
     return new Promise((resolve, reject) => {
       gm(buffer)
         .quality(92)
-        .sharpen(2, 1)
+        .sharpen(1.5, 1)
         .resize(width, metadata.height * (width / metadata.width), '!')
-        .crop(width - 4 * multiplier, height - 4 * multiplier, 0, 0)
+        .crop(width, height, 0, 0)
         .background('#ffffff')
-        .extent(width - 4 * multiplier, height - 4 * multiplier)
-        .borderColor('white')
-        .border(2 * multiplier, 2 * multiplier)
+        .extent(width, height)
         .flatten()
-        .toBuffer(isGif(buffer) ? 'gif' : 'jpg', (err, buf) => {
+        .toBuffer(isImageType(buffer, MAGIC.gifNumber) ? 'gif' : 'jpg', (err, buf) => {
           if (err) return reject(err);
           return resolve(buf);
         });
     });
   }
-  if (roundedRatio <= 0.64) {
+  if (roundedRatio <= 0.64 && !isImageType(buffer, MAGIC.gifNumber)) {
     return new Promise((resolve, reject) => {
       gm(buffer)
         .quality(92)
-        .sharpen(2, 1)
+        .sharpen(1.5, 1)
         .gravity('Center')
         .resize(width, metadata.height * (width / metadata.width), '!')
-        .crop(width - 4 * multiplier, height - 4 * multiplier, 0, 0)
+        .crop(width, height, 0, 0)
         .background('#ffffff')
-        .extent(width - 4 * multiplier, height - 4 * multiplier)
-        .borderColor('white')
-        .border(2 * multiplier, 2 * multiplier)
+        .extent(width, height)
         .flatten()
-        .toBuffer(isGif(buffer) ? 'gif' : 'jpg', (err, buf) => {
+        .toBuffer(isImageType(buffer, MAGIC.gifNumber) ? 'gif' : 'jpg', (err, buf) => {
           if (err) return reject(err);
           return resolve(buf);
         });
@@ -148,8 +155,8 @@ const execute = async (url, width, height, userOptions) => {
 
   const { topCrop: crop } = await smartcrop.crop(buffer, options);
 
-  if (isGif(buffer)) {
-    return promiseGM(buffer, crop, width, height, multiplier, true);
+  if (isImageType(buffer, MAGIC.gifNumber)) {
+    return promiseGM(buffer, crop, width, height, true);
   }
 
   if (roundedRatio > 1.7) {
@@ -160,11 +167,9 @@ const execute = async (url, width, height, userOptions) => {
     return new Promise((resolve, reject) => {
       gm(sharpBuffer)
         .quality(92)
-        .sharpen(2, 1)
+        .sharpen(1.5, 1)
         .background('#ffffff')
-        .extent(width - 4 * multiplier, height - 4 * multiplier)
-        .borderColor('white')
-        .border(2 * multiplier, 2 * multiplier)
+        .extent(width, height)
         .flatten()
         .toBuffer('jpg', (err, buf) => {
           if (err) return reject(err);
@@ -173,7 +178,7 @@ const execute = async (url, width, height, userOptions) => {
     });
   }
 
-  return promiseGM(buffer, crop, width, height, multiplier);
+  return promiseGM(buffer, crop, width, height);
 };
 
 
