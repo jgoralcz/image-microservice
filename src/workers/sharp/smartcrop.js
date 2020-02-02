9997f135b88baf7685e6fd51bba523753db7c114
jgoralcz <jgoralcz@asu.edu>
2020-02-02 14:53:34 -0700

    test with smartcrop

--- src/workers/sharp/smartcrop.js
+++ src/workers/sharp/smartcrop.js
@@ -65,9 +65,9 @@ const promiseGM = (buffer, crop, width, height, gif, boost) => new Promise((reso
   }
 
   const bufferGM = gm(buffer);
-  if (boost && boost.length <= 0 && crop && crop.score && crop.score.skin < 8 && crop.score.detail < 5 && (crop.score.saturation < -2 || crop.score.skin < -1 || crop.score.detail < -24)) {
-    bufferGM.gravity('Center');
-  }
+  // if (boost && boost.length <= 0 && crop && crop.score && crop.score.skin < 8 && crop.score.detail < 5 && (crop.score.saturation < -2 || crop.score.skin < -1 || crop.score.detail < -24)) {
+  //   bufferGM.gravity('Center');
+  // }
   bufferGM.crop(crop.width, crop.height, crop.x, crop.y)
     .resize(width, height, '!')
     .flatten()
